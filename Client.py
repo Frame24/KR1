d497462fb3499feb4ee6af531dd8856235c806cb
Frame24 <hks9@yandex.ru>
2023-05-18 15:15:18 +0500

    Закомментил аплод файла, сделал так, чтобы всегда выбирался титаник

--- Client.py
+++ Client.py
@@ -3,44 +3,45 @@ import pandas as pd
 
 
 def main():
-    uploaded_file = st.file_uploader(
-        "Choose a CSV file",
-        accept_multiple_files=False)
-    if uploaded_file:
-        st.header("Задание 1:")
-        isSurvived = st.radio(
-            label="Выжили ли:",
-            options=["Выжили", "Погибли"])
-        age = st.radio(label="Возраст:", options=["<30", ">60"])
+    # uploaded_file = st.file_uploader(
+    #     "Choose a CSV file",
+    #     accept_multiple_files=False)
+    # if uploaded_file:
+    st.header("Задание 1:")
+    isSurvived = st.radio(
+        label="Выжили ли:",
+        options=["Выжили", "Погибли"])
+    age = st.radio(label="Возраст:", options=["<30", ">60"])
 
-        st.write("Результат:")
-        df = load_dataframe(uploaded_file)
-        young_survived, old_survived = task1(df, "Age", 30, 60, "Survived")
+    st.write("Результат:")
+    # df = load_dataframe(uploaded_file)
+    df = load_dataframe(pd.read_csv("./titanic_train.csv"))
+    young_survived, old_survived = task1(df, "Age", 30, 60, "Survived")
 
-        if isSurvived == "Выжили":
-            if age == "<30":
-                result_mean = 100 * young_survived.mean()
-            if age == ">60":
-                result_mean = 100 * old_survived.mean()
-        else:
-            if age == "<30":
-                result_mean = 100 * (1 - young_survived.mean())
-            if age == ">60":
-                result_mean = 100 * (1 - old_survived.mean())
+    if isSurvived == "Выжили":
+        if age == "<30":
+            result_mean = 100 * young_survived.mean()
+        if age == ">60":
+            result_mean = 100 * old_survived.mean()
+    else:
+        if age == "<30":
+            result_mean = 100 * (1 - young_survived.mean())
+        if age == ">60":
+            result_mean = 100 * (1 - old_survived.mean())
 
-        st.write(str(round(result_mean, 2)) + "%")
+    st.write(str(round(result_mean, 2)) + "%")
 
-        st.header("Задание 2:")
+    st.header("Задание 2:")
 
-        Sex = st.radio(label="Пол:", options=["Мужской", "Женский"])
-        st.write("Результат:")
+    Sex = st.radio(label="Пол:", options=["Мужской", "Женский"])
+    st.write("Результат:")
 
-        if Sex == "Мужской":
-            result_sex_mean = 100 * task2(df, "male", True)
-        elif Sex == "Женский":
-            result_sex_mean = 100 * task2(df, "female", True)
+    if Sex == "Мужской":
+        result_sex_mean = 100 * task2(df, "male", True)
+    elif Sex == "Женский":
+        result_sex_mean = 100 * task2(df, "female", True)
 
-        st.write(str(round(result_sex_mean, 2)) + "%")
+    st.write(str(round(result_sex_mean, 2)) + "%")
 
 
 def load_dataframe(data):
